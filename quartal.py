f3f8ab3a1e370b470000c1785b49e6189f1929c2
Kristopher Johnson <kris@kristopherjohnson.net>
2023-11-06 18:26:48 -0500

    Fix fret numbers for quartal.py

--- quartal.py
+++ quartal.py
@@ -24,8 +24,8 @@ chords = [
     { 'label': 'C', 'color': 'brown',  'markers': [(1,  3), (2,  3), (3,  4), (4,  5)] },
     { 'label': 'D', 'color': 'green',  'markers': [(1,  5), (2,  5), (3,  5), (4,  6)] },
     { 'label': 'E', 'color': 'blue',   'markers': [(1,  7), (2,  7), (3,  7), (4,  8)] },
-    { 'label': 'F', 'color': 'indigo', 'markers': [(1,  9), (2, 10), (3, 10), (4, 11)] },
-    { 'label': 'G', 'color': 'violet', 'markers': [(1, 11), (2, 11), (3, 11), (4, 13)] },
+    { 'label': 'F', 'color': 'indigo', 'markers': [(1,  8), (2,  9), (3,  9), (4, 10)] },
+    { 'label': 'G', 'color': 'violet', 'markers': [(1, 10), (2, 10), (3, 10), (4, 12)] },
 ]
 
 # Render in reverse order so that when chords overlap, the higher chord's color
@@ -37,7 +37,7 @@ for chord in chords:
     for (string, fret) in markers:
         note_label = label if string == 1 else None
         fb.add_marker(label=note_label, string=string, fret=fret, color=color)
-        if fret < 12:
+        if fret <= 12:
             fb.add_marker(label=note_label, string=string, fret=fret+12, color=color)
 
 fb.save('quartal.svg')
